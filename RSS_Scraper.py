f64bb86751e283a493a8077d060f9fa1e8d45744
@LeX <ghebaura34@gmail.com>
2023-11-26 17:06:48 +0200

    Update RSS_Scraper.py

--- RSS_Scraper.py
+++ RSS_Scraper.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
+import xml.dom.minidom
 
 def get_game_description(game_url):
     try:
         response = requests.get(game_url)
         if response.status_code == 200:
             soup = BeautifulSoup(response.content, 'html.parser')
-            # Extrage descrierea jocului
             description = soup.find('div', class_='game_description_snippet')
             return description.text.strip() if description else 'Nicio descriere disponibilă'
     except Exception as e:
@@ -20,9 +20,8 @@ def get_game_image_url(game_url):
             soup = BeautifulSoup(response.content, 'html.parser')
             image = soup.find('img', class_='game_header_image_full')
             if image:
-                # Elimină parametrii de query de după '.jpg'
                 image_url = image['src']
-                jpg_end_index = image_url.find('.jpg') + 4  # '+ 4' pentru a include '.jpg'
+                jpg_end_index = image_url.find('.jpg') + 4
                 return image_url[:jpg_end_index] if jpg_end_index > 4 else None
             else:
                 return None
@@ -54,8 +53,6 @@ def generate_rss_feed(games_data):
     rss = ET.Element('rss', xmlns_atom="http://www.w3.org/2005/Atom", xmlns_content="http://purl.org/rss/1.0/modules/content/", version="2.0")
     channel = ET.SubElement(rss, 'channel')
 
-    # Adăugarea elementelor suplimentare (titlu, link, etc.) dacă este necesar
-
     for game_title, game_link, game_description, game_image in games_data:
         item = ET.SubElement(channel, 'item')
         title_element = ET.SubElement(item, 'title')
@@ -63,16 +60,19 @@ def generate_rss_feed(games_data):
         link_element = ET.SubElement(item, 'link')
         link_element.text = game_link
         description_element = ET.SubElement(item, 'description')
-        description_with_image = f'<![CDATA[<img src="{game_image}" /><br/>{game_description}]]>' if game_image else f'<![CDATA[{game_description}]]'
-        description_element.text = description_with_image
+        if game_image:
+            description_with_html = f'<![CDATA[<img src="{game_image}" /><br/>{game_description}]]>'
+        else:
+            description_with_html = f'<![CDATA[{game_description}]]>'
+        description_element.text = description_with_html
 
-    tree = ET.ElementTree(rss)
-    tree.write('docs/index.xml', encoding='UTF-8', xml_declaration=True)
+    rough_string = ET.tostring(rss, 'utf-8')
+    reparsed = xml.dom.minidom.parseString(rough_string)
+    with open('docs/index.xml', 'w', encoding='UTF-8') as f:
+        f.write(reparsed.toprettyxml(indent="\t"))
 
-# URL-ul paginii cu noile lansări Steam
+
 url = 'https://store.steampowered.com/explore/new/'
-
-# Execută scraping-ul și generează feed-ul RSS
 games_data = scrape_steam_new_releases(url)
 if games_data:
     generate_rss_feed(games_data)
