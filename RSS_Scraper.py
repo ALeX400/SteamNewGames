d455050946b1448611c0a29c52472bafee5fd941
@LeX <ghebaura34@gmail.com>
2023-11-26 19:41:56 +0200

    Update RSS_Scraper.py

--- RSS_Scraper.py
+++ RSS_Scraper.py
@@ -1,52 +1,57 @@
-import requests
+import aiohttp
+import asyncio
 from bs4 import BeautifulSoup
 from lxml import etree
 
-def get_game_description(game_url):
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+async def get_game_description(session, game_url):
     try:
-        response = requests.get(game_url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.content, 'html.parser')
-            description = soup.find('div', id='game_area_description', class_='game_area_description')
-            if description:
-                # Adaugă un stil pentru a centra textul
-                description_html = f'<div style="text-align: center;">{description}</div>'
-                return description_html
-            else:
-                return 'Nicio descriere disponibilă'
+        html = await fetch(session, game_url)
+        soup = BeautifulSoup(html, 'html.parser')
+        description = soup.find('div', id='game_area_description', class_='game_area_description')
+        if description:
+            description_html = f'<div style="text-align: center;">{description}</div>'
+            return description_html
+        else:
+            return 'Nicio descriere disponibilă'
     except Exception as e:
         return 'Eroare la extragerea descrierii: ' + str(e)
 
-
-def get_game_image_url(game_url):
+async def get_game_image_url(session, game_url):
     try:
-        response = requests.get(game_url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.content, 'html.parser')
-            image = soup.find('img', class_='game_header_image_full')
-            if image:
-                image_url = image['src']
-                jpg_end_index = image_url.find('.jpg') + 4
-                return image_url[:jpg_end_index] if jpg_end_index > 4 else None
-            else:
-                return None
+        html = await fetch(session, game_url)
+        soup = BeautifulSoup(html, 'html.parser')
+        image = soup.find('img', class_='game_header_image_full')
+        if image:
+            image_url = image['src']
+            jpg_end_index = image_url.find('.jpg') + 4
+            return image_url[:jpg_end_index] if jpg_end_index > 4 else None
+        else:
+            return None
     except Exception as e:
         return None
 
-def scrape_steam_new_releases(url):
-    response = requests.get(url)
-    if response.status_code != 200:
-        return None
-
-    soup = BeautifulSoup(response.content, 'html.parser')
+async def scrape_steam_new_releases(session, url):
+    html = await fetch(session, url)
+    soup = BeautifulSoup(html, 'html.parser')
     new_releases = soup.find_all('a', class_='tab_item')
 
     games_data = []
     for game in new_releases:
         title = game.find('div', class_='tab_item_name').text
         game_url = game['href']
-        description = get_game_description(game_url)
-        image_url = get_game_image_url(game_url)
+
+        # Crează task-uri asincrone pentru fiecare joc
+        description_task = asyncio.create_task(get_game_description(session, game_url))
+        image_task = asyncio.create_task(get_game_image_url(session, game_url))
+
+        # Așteaptă finalizarea task-urilor
+        description = await description_task
+        image_url = await image_task
+
         games_data.append((title, game_url, description, image_url))
 
     return games_data
@@ -73,9 +78,14 @@ def generate_rss_feed(games_data):
     with open('docs/index.xml', 'wb') as f:
         f.write(etree.tostring(rss, pretty_print=True, xml_declaration=True, encoding='UTF-8'))
 
-url = 'https://store.steampowered.com/explore/new/'
-games_data = scrape_steam_new_releases(url)
-if games_data:
-    generate_rss_feed(games_data)
-else:
-    print("Nu s-au putut extrage datele de pe Steam.")
+async def main():
+    async with aiohttp.ClientSession() as session:
+        url = 'https://store.steampowered.com/explore/new/'
+        games_data = await scrape_steam_new_releases(session, url)
+        if games_data:
+            generate_rss_feed(games_data)
+        else:
+            print("Nu s-au putut extrage datele de pe Steam.")
+
+if __name__ == "__main__":
+    asyncio.run(main())
