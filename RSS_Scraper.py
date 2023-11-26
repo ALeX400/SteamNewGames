cda2d13ce1236b36fc5c3097e47bccda8a883287
@LeX <ghebaura34@gmail.com>
2023-11-26 16:03:26 +0200

    Update RSS_Scraper.py

--- RSS_Scraper.py
+++ RSS_Scraper.py
@@ -7,11 +7,21 @@ def get_game_description(game_url):
         response = requests.get(game_url)
         if response.status_code == 200:
             soup = BeautifulSoup(response.content, 'html.parser')
-            # Extrage descrierea jocului - aceasta va varia în funcție de structura paginii
+            # Extrage descrierea jocului
             description = soup.find('div', class_='game_description_snippet')
             return description.text.strip() if description else 'Nicio descriere disponibilă'
     except Exception as e:
         return 'Eroare la extragerea descrierii: ' + str(e)
+
+def get_game_image_url(game_url):
+    try:
+        response = requests.get(game_url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            image = soup.find('img', class_='game_header_image_full')
+            return image['src'] if image else None
+    except Exception as e:
+        return None
 
 def scrape_steam_new_releases(url):
     response = requests.get(url)
@@ -26,7 +36,8 @@ def scrape_steam_new_releases(url):
         title = game.find('div', class_='tab_item_name').text
         game_url = game['href']
         description = get_game_description(game_url)
-        games_data.append((title, game_url, description))
+        image_url = get_game_image_url(game_url)
+        games_data.append((title, game_url, description, image_url))
 
     return games_data
 
@@ -39,7 +50,7 @@ def generate_rss_feed(games_data):
     description = ET.SubElement(channel, 'description')
     description.text = 'Latest game releases on Steam'
 
-    for game_title, game_link, game_description in games_data:
+    for game_title, game_link, game_description, game_image in games_data:
         item = ET.SubElement(channel, 'item')
         title_element = ET.SubElement(item, 'title')
         title_element.text = game_title
@@ -47,6 +58,11 @@ def generate_rss_feed(games_data):
         link_element.text = game_link
         description_element = ET.SubElement(item, 'description')
         description_element.text = game_description
+
+        if game_image:
+            image_element = ET.SubElement(item, 'enclosure')
+            image_element.set('url', game_image)
+            image_element.set('type', 'image/jpeg')  # Asumând că imaginile sunt în format JPEG
 
     tree = ET.ElementTree(rss)
     tree.write('docs/index.xml', encoding='utf-8', xml_declaration=True)
